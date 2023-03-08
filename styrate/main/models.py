aa4b21f0b1edeb9a767053cd76d5ea900241a284
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-08 11:48:50 +0000

    review list updates and header ui

--- styrate/main/models.py
+++ styrate/main/models.py
@@ -11,9 +11,13 @@ class Review(models.Model):
     id = models.AutoField(primary_key=True, editable=True)
     dateCreated = models.DateTimeField(auto_now_add=True, editable=True)
     title = models.CharField(max_length=100, editable=True, null=True)
+    overview = models.CharField(max_length=100, editable=True, null=True)
     textField = models.CharField(max_length=1000, editable=True, blank=True, null=True)
+    itemCategory = models.CharField(max_length=20, editable=True, null=True)
     image = models.ImageField(editable=True, blank=True, null=True)
     videoID = models.CharField(max_length=100, editable=True)
+    productName = models.CharField(max_length=25, editable=True, null=True)
+    itemLink = models.CharField(max_length=150, editable=True, null=True)
     createdByUser_Key = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ReviewsCreatedBy_List')
 
 class Follow(models.Model):
