162f27e895ced69210c03519aa23494c420dccd6
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-12 12:56:50 +0000

    like count logic

--- styrate/main/controllers/General.py
+++ styrate/main/controllers/General.py
@@ -1,10 +1,18 @@
-from ..models import Follow,User
+from ..models import *
 
 class GeneralController:
     def AccountInformation(request, userObject):
         tempObj = userObject
         # Finding whether the current user is the owner 
         tempObj.isEqual = True if request.user.is_authenticated and request.user.id==userObject.id else False
+        # Finding whether the user follows them
         tempObj.following = True if request.user.is_authenticated and Follow.objects.filter(followFromUser_Key=User.objects.get(id=request.user.id), followToUser_Key=userObject).exists()else False
-        # tempObj.follow_object_id = Follow.objects.get(followFromUser_Key=User.objects.get(id=request.user.id), followToUser_Key=userObject).id if tempObj.following else None
+        # Finding whether user's stats
+        if request.user.is_authenticated:
+            tempObj.numReviews = len(userObject.ReviewsCreatedBy_List.all())
+            tempObj.numFollowers = len(userObject.FollowedBy_List.all())
+            tempObj.numFollowing = len(userObject.Follows_List.all())
+            # Finding the number of likes
+            users_ReviewObjects = userObject.ReviewsCreatedBy_List.all()
+            tempObj.likeCount  = len(Like.objects.filter(onReview_Key__in = users_ReviewObjects))
         return tempObj
